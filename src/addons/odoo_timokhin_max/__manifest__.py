e97e01a3041b241e1648267b2204fde23e8b3d90
Maxon Kruasson <ma_timokhin@mail.ru>
2026-03-22 15:06:03 +0300

    Initial commit

---
+++ src/addons/odoo_timokhin_max/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name' : 'odoo_timokhin_max',
+    'version' : '1.0',
+    'depends' : ['sale','hr'],
+    'data' : ['views/sale_order_views.xml',
+              'views/sale_order_report.xml'
+             ],
+    'installable' : True,
+    'application' : False,
+}
---
+++ src/addons/odoo_timokhin_max/models/sale_order.py
@@ -0,0 +1,50 @@
+import datetime
+
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+import random
+import string
+
+class SaleOrderInherit(models.Model):
+    _inherit = 'sale.order'
+
+    def _random_letters(self):
+        return ''.join(random.choices(string.ascii_uppercase, k=10))
+
+
+    responsible_employee = fields.Many2one('hr.employee', string='Ответственный за выдачу', required=True)
+    new_field_total = fields.Char(string='New Field', default = _random_letters, copy=False)
+
+
+    @api.onchange('date_order', 'order_line', 'fiscal_position_id')
+    def _onchange_date_price_update(self):
+
+            if self.date_order:
+                now = datetime.datetime.now()
+                if self.date_order.strftime('%d.%m.%Y%H:%M:%S') != now.strftime('%d.%m.%Y%H:%M:%S'):
+                    date_str = self.date_order.strftime('%d.%m.%Y %H:%M:%S')
+                    amount = self.amount_total or 0.0
+                    self.new_field_total = f"{date_str} + {amount:.2f}"
+
+    @api.onchange('new_field_total')
+    def _onchange_field_len(self):
+        if self.new_field_total and len(self.new_field_total) > 30:
+            return {
+                'warning':{
+                'title':"Превышена максимальная длина поля",
+                'mesage': f"Длина текста должна быть меньше 30 символов!",
+            },
+            }
+    @api.constrains('new_field_total')
+    def _constrains_new_field(self):
+      if self.new_field_total and len(self.new_field_total) > 30:
+            for record in self:
+                if record.new_field_total and len(record.new_field_total) > 30:
+                    raise ValidationError(
+                        f"Длина текста должна быть меньше 30 символов!"
+                    )
+
+
+
+
+                #,  'fiscal_position_id'
